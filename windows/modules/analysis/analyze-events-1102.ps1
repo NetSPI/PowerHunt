ebf25669202ab3f2afc089cbefc8d34b2f83f4af
Scott Sutherland <nullbind@users.noreply.github.com>
2022-03-23 08:22:48 -0500

    Add windows modules

---
+++ windows/modules/analysis/analyze-events-1102.ps1
@@ -0,0 +1,12 @@
+﻿
+# Script : Invoke-PowerHunt
+# Module : analyze-events-1102-count
+# Version: 1.0
+# Author : Scott Sutherland
+# Summary: This is script is part of the PowerHunt framework.
+# License: 3-clause BSD
+
+
+# Save result summary
+$AnalysisModuleFileName = $_.name -replace(".ps1","-counts.csv")
+$CollectedData  | group PrincipalName | Sort-Object count -Descending | select count,name | Export-Csv -NoTypeInformation "$OutputDirectory\analysis\Hunt-$AnalysisModuleFileName"
