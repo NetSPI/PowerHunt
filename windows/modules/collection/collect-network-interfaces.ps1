484e492861683954aa2d16b29a2c4ceedf5fb5a2
Scott Sutherland <nullbind@users.noreply.github.com>
2022-03-24 20:12:44 -0500

    Update and rename collect-network-interface.ps1 to collect-network-interfaces.ps1

--- windows/modules/collection/collect-network-interfaces.ps1
+++ windows/modules/collection/collect-network-interfaces.ps1
@@ -1,6 +1,6 @@
 
 # Script : Invoke-PowerHunt
-# Module : collect-network-interface
+# Module : collect-network-interfaces
 # Version: 1.0
 # Author : Scott Sutherland
 # Summary: This is script is part of the PowerHunt framework and collect connection information.
