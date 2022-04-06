d13c6ec5853a5f737f9d1b61587ce628d51ad0d0
Scott Sutherland <nullbind@users.noreply.github.com>
2022-04-06 14:19:49 -0500

    Add files via upload

---
+++ windows/modules/analysis/stack/analyze-001-01-connections-process-paths.ps1
@@ -0,0 +1,24 @@
+﻿
+# Script : Invoke-PowerHunt
+# Module : analyze-connections-process-paths
+# Version: 1.0
+# Author : Scott Sutherland
+# Summary: This is script is part of the PowerHunt framework.
+# License: 3-clause BSD
+
+$AnalysisModuleDesc = "Place holder description."
+
+
+# Save result summary
+$AnalysisModuleFileName = $_.name -replace(".ps1","ProcessPath-counts.csv")
+$FinalOutput = $CollectedData  | group FilePath | Sort-Object count -Descending | select count,name 
+$FinalOutput | Export-Csv -NoTypeInformation "$OutputDirectory\analysis\$AnalysisSubDir\Hunt-$AnalysisModuleFileName"
+
+# Count instances
+$InstanceCount = $FinalOutput | measure | select count -expandproperty count
+
+# Count affected computers 
+$AnalysisModuleAffectedComputerCount = $FinalOutput | select PSComputerName -Unique | measure | select count -ExpandProperty count                    
+
+# Save summary metrics
+$null = $ModuleOutputSummary.Rows.Add("$ModuleType","$CollectionModuleName","$CollectionDataSource","$AnalysisModuleName","$AnalysisModuleDesc","$AnalysisType","$InstanceCount","$AnalysisModuleAffectedComputerCount")
